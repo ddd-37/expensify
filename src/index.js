00cf7d92f32e3c981a0656ea1ec4dae7b80b36dd
Devon Deason <devon.d.deason@gmail.com>
2020-01-07 14:33:23 -0700

    setup NavLinks and node-sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import "normalize.css/normalize.css";
+import "./styles/style.scss";
 
 const ExpenseDashboardPage = () => <div>This the dashboard</div>;
 
@@ -11,17 +12,45 @@ const EditExpensePage = () => <div>This the edit expense component</div>;
 
 const HelpPage = () => <div>This the help component</div>;
 
-const NotFoundPage = () => <div>404!</div>;
+const NotFoundPage = () => (
+  <div>
+    404! <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <span> </span>
+    <NavLink to="/create" activeClassName="is-active" exact={true}>
+      Create Expense
+    </NavLink>
+    <span> </span>
+    <NavLink to="/edit" activeClassName="is-active" exact={true}>
+      Edit Page
+    </NavLink>
+    <span> </span>
+    <NavLink to="/help" activeClassName="is-active" exact={true}>
+      Help Page
+    </NavLink>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </>
   </BrowserRouter>
 );
 
