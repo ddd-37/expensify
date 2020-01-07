02479af305b2aff3c743319fbf381caf14247e28
Devon Deason <devon.d.deason@gmail.com>
2020-01-07 14:04:26 -0700

    setup routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const ExpenseDashboardPage = () => <div>This the dashboard</div>;
+
+const AddExpensePage = () => <div>This the add expense component</div>;
+
+const EditExpensePage = () => <div>This the edit expense component</div>;
+
+const HelpPage = () => <div>This the help component</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("root"));
