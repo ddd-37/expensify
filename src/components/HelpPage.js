9b89fd73462b9d09d62501397ad02d7a03a1fbba
Devon Deason <devon.d.deason@gmail.com>
2020-04-14 07:16:50 -0600

    create public route

--- src/components/HelpPage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const HelpPage = () => <div>This the help component</div>;
-
-export default HelpPage;
---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+import Header from "./../components/Header";
+
+const PublicRoute = (props) =>
+  !props.isAuthenticated ? (
+    <>
+      <Header />
+      <Route {...props} />
+    </>
+  ) : (
+    <Redirect to="/dashboard" />
+  );
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: !!state.auth.uid,
+});
+
+export default connect(mapStateToProps)(PublicRoute);
