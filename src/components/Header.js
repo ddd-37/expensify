fe3a53dab5bd7cbfe8579ee00f3bd101b22836dd
Devon Deason <devon.d.deason@gmail.com>
2020-04-14 07:21:08 -0600

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ startLogOut }) => {
   return (
     <header>
       <h1>Expensify</h1>
-      <NavLink to="/" activeClassName="is-active" exact={true}>
+      <NavLink to="/dashboard" activeClassName="is-active">
         Dashboard
       </NavLink>
       <span> </span>
